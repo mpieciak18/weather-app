89dab10b401582c2fa32ba1a0cddaccf04daf834
Mark Pieciak <mpieciak18@msn.com>
2021-07-21 08:44:02 -0400

    fetch current forecast in Fahrenheit & assign certain attributes to object constructor

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+// Async function that makes API request
 const fetchWeather = async function() {
-    const url = 'http://api.openweathermap.org/data/2.5/weather?q=Hazlet,%20NJ,%20US&APPID=7c6798fe35f78605f76d08c63b03e3c6';
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q=Hazlet,%20NJ,%20US&units=imperial&APPID=7c6798fe35f78605f76d08c63b03e3c6';
     
     try {
-        const response = await fetch(url, {mode: 'cors'})
-        const modResponse = await response.json();
-        console.log(modResponse);
+        const todayResponse = await fetch(url, {mode: 'cors'})
+        const todayJson = await todayResponse.json();
+        console.log(todayJson);
+        const todayWeather = new todayWeatherConst(todayJson);
+        console.log(todayWeather);
     } catch {
         console.log('error!');
     };
-}
+};
+// Creates today's weather forecast object with only necessary attributes
+const todayWeatherConst = function(object) {
+    this.weather_summ = object.weather[0].main;
+    this.weather_descrip = object.weather[0].description;
+    this.weather_icon = object.weather[0].description;
+    // Contains temp, feels_like, temp_min, temp_max, pressure, & humidity
+    this.main = object.main;
+    this.visibility = object.visibility;
+    // Contains speed & deg
+    this.wind = object.wind;
+    this.city = object.name;
+    this.country = object.sys.country;
+};
 
 fetchWeather();
