088e1f94d4f2287c8ab28ff7234d1df45cd9ba5f
Mark Pieciak <mpieciak18@msn.com>
2021-07-29 07:34:06 -0400

    change API to OpenWeather One Call, fetch weather using coordinates, and update current weather object constructor

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-// Async function that makes API request
+const appId = '7c6798fe35f78605f76d08c63b03e3c6';
+
+// Async function that fetches current weather from OpenWeather's API
 const fetchWeather = async function() {
-    const url = 'http://api.openweathermap.org/data/2.5/weather?q=Hazlet,%20NJ,%20US&units=imperial&APPID=7c6798fe35f78605f76d08c63b03e3c6';
-    
+    // Set default parameters
+    let lat = '';
+    let lon = '';
+    let exclusion = 'minutely,hourly,daily,alerts';
+    let units = 'imperial';
     try {
+        const coords = await fetchCoords();
+        lat = coords.lat;
+        lon = coords.lon;
+        const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclusion}&units=${units}&appid=${appId}`;
         const todayResponse = await fetch(url, {mode: 'cors'})
         const todayJson = await todayResponse.json();
-        console.log(todayJson);
-        const todayWeather = new todayWeatherConst(todayJson);
+        const todayWeather = new todayWeatherConst(todayJson.current);
         console.log(todayWeather);
     } catch {
         console.log('error!');
     };
 };
+// Async function that is called by fetchWeather function to fetch location coordinates
+const fetchCoords = async function() {
+    // Set location to Hazlet, NJ by default
+    let location = 'Hazlet,%20NJ,%20US'
+    try {
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${appId}`;
+        const response = await fetch(url, {mode: 'cors'})
+        const responseJson = await response.json();
+        return responseJson.coord
+    } catch {
+        console.log('error!');
+    };
+}
 // Creates today's weather forecast object with only necessary attributes
 const todayWeatherConst = function(object) {
+    // Summarical attributes
     this.weather_summ = object.weather[0].main;
     this.weather_descrip = object.weather[0].description;
-    this.weather_icon = object.weather[0].description;
-    // Contains temp, feels_like, temp_min, temp_max, pressure, & humidity
-    this.main = object.main;
+    this.weather_icon = object.weather[0].icon;
+    // All other attributes
+    this.sunrise = object.sunrise;
+    this.sunset = object.sunset;
+    this.temp = object.temp;
+    this.feels_like = object.feels_like;
+    this.pressure = object.pressure;
+    this.humidity = object.humidity;
+    this.dew_point = object.dew_point;
+    this.uvi = object.uvi;
+    this.clouds = object.clouds;
     this.visibility = object.visibility;
-    // Contains speed & deg
-    this.wind = object.wind;
-    this.city = object.name;
-    this.country = object.sys.country;
+    this.wind_speed = object.wind_speed;
+    this.wind_deg = object.wind_deg;
 };
 
 fetchWeather();
